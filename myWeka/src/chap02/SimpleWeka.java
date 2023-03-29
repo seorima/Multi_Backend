de77bca4984e611740173a6ed179b37311021189
Seorim <seorima@naver.com>
2023-03-29 13:37:46 +0900

    weka

--- myWeka/src/chap02/SimpleWeka.java
+++ myWeka/src/chap02/SimpleWeka.java
@@ -1,24 +1,39 @@
 package chap02;
 
 import java.io.*;
+import java.util.*;
 
-import weka.core.Debug.Random;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 
 public class SimpleWeka {
-	
-	
+
 	public static void main(String [] args) throws Exception {
 		int numfolds = 10;
 		int numfold = 0;
-		int seed =1 ;
+		int seed = 1;
 		
+		Instances data = new Instances(new BufferedReader(new FileReader(
+															"C:/projects/Weka-3-9-6/weka-main/data/iris.arff")));
 		
-		Instances data =  new Instances(new BufferedReader(new FileReader("C:/projects/Weka-3-9-6/data/iris.arff")));
+		Instances train = data.trainCV(numfolds, numfold, new Random(seed)); //훈련데이터
+		Instances test = data.testCV(numfolds, numfold); // 검증데이터
 		
-		Instances train = data.trainCV(numfolds,numfold, new Random(seed)); // 훈련데이터
-		Instances test = data.testCV(numfolds,numfold); // 검증데이터
+		RandomForest model = new RandomForest();
 		
+		train.setClassIndex(train.numAttributes() - 1);
+		test.setClassIndex(train.numAttributes() - 1);
+		
+		Evaluation eval = new Evaluation(train);
+		eval.crossValidateModel(model, train, numfolds, new Random(seed));
+		
+		model.buildClassifier(train);//모델 학습 시작
+		
+		eval.evaluateModel(model, test); // 모델에 대한 평가 시작
+		
+		System.out.println(model);
+		System.out.println(eval.toSummaryString());
+		System.out.println(eval.toMatrixString());
 	}
-
 }
---
+++ myWeka/src/chap02/simpleLinearRegresion.java
@@ -0,0 +1,11 @@
+package chap02;
+
+public class simpleLinearRegresion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 최소제곱법을 이용하여 a를 구하는 코드를 작성하세요.
+		
+	}
+
+}
