59b041cc4d413cf0fa46df63f33e0e21ab0b2ad3
Seorim <seorima@naver.com>
2023-04-24 18:14:11 +0900

    why

--- mySpringWeb/src/main/java/chap11/exception/DuplicateMemberException.java
+++ mySpringWeb/src/main/java/chap11/exception/DuplicateMemberException.java
@@ -2,5 +2,5 @@ package chap11.exception;
 
 public class DuplicateMemberException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-
+sdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf
 }
